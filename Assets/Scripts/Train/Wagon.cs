e7d43466cbdd1b0259523c55140192f8da8c1f79
Traxx186 <justinvanderkruit@icloud.com>
2021-06-12 23:17:11 +0200

    Wagons can now move from point to point

--- Assets/Scripts/Train/Wagon.cs
+++ Assets/Scripts/Train/Wagon.cs
@@ -7,6 +7,11 @@ public class Wagon : MonoBehaviour
     [SerializeField]
     private Wagon prevWagon, nextWagon;
 
+    [SerializeField]
+    private Train train; 
+    
+    private Waypoint nextWaypoint;
+
     void Start()
     {
         //TODO: uncomment when wagons get generated automatically
@@ -15,7 +20,15 @@ public class Wagon : MonoBehaviour
 
     private void Update()
     {
+        float step = Time.deltaTime * train.Speed;
+        Waypoint waypoint = (null != nextWaypoint) ? nextWaypoint : train.StartPoint.NextWaypoint();
+        Vector2 difference = transform.position - waypoint.Position;
+
+        transform.rotation = Quaternion.Euler(0, 0, Vector2.SignedAngle(Vector3.up, difference) - train.Offset);
+        transform.position = Vector3.MoveTowards(transform.position, waypoint.Position, step);
         
+        if (Vector3.Distance(transform.position, waypoint.Position) < 0.01f && null != waypoint.NextWaypoint())
+            nextWaypoint = waypoint.NextWaypoint();
     }
 
     [ContextMenu("Couples wagon")]
